12ca545ea586eb4a1a5f8835531e053a2c769af3
고나경 <zciop@naver.com>
2026-09-02 09:37:21 +0900

    동일 와이파이 사용 타인 내 서버 접근 가능하게 수정

--- config/settings.py
+++ config/settings.py
@@ -4,7 +4,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = "personal-spending-tracker-local-dev-key"
 DEBUG = True
-ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
+ALLOWED_HOSTS = ["127.0.0.1", "localhost", "*"]
 
 INSTALLED_APPS = [
     "django.contrib.admin",
